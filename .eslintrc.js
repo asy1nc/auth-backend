8ea8e3c46092ba6fa73bab63bc9dc69587092892
asy1nc <sabamksh@gmail.com>
2024-01-19 06:27:21 +0400

    chore/ eslint prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
+    ]
+  };
